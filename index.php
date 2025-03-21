3810a44938bbd568bf6a711778f352a95598456b
AndreaRaga56 <andrearaga56@gmail.com>
2025-03-21 18:12:24 +0100

    feat style

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Php Hotel</title>
 </head>
 <body>
@@ -50,13 +51,44 @@
 
     ];
 
+    echo '<h1>Hotels</h1>';
 
-    foreach ($hotels as $hotel) {
-        foreach ($hotel as $key => $value) {
-            echo $key . ': ' . $value . '<br>';
+    echo '<div><div class="row mt-5 mb-1">';
+    //Printer della tabella
+    for ($i=0; $i < count($hotels); $i++) {
+        //Printer dell'intestazione della tabella
+        if ($i==0){
+            foreach ($hotels["$i"] as $key => $value) {
+                echo "<div class='col-2'><strong>" . ucwords($key) . "</strong></div>";
+            };
+            echo '</div>';
+        };
+        echo '<div class="row">';
+
+        //Printer del contenuto della tabella
+        foreach ($hotels["$i"] as $key => $value) {   
+            if ($value===true||$value===false){
+                if ($value===true){
+                echo "<div class='col-2'>Presente</div>";
+                } else{
+                    echo "<div class='col-2'>Assente</div>";
+                }
+            } else {      
+            echo "<div class='col-2'>" . ucwords($value) . "</div>"; 
+            }        
         }
-        echo '<br>';
+        echo '</div>';
     }
+
+    
+
+    // foreach ($hotels as $hotel) {
+    //     foreach ($hotel as $key => $value) {
+    //         echo "<div> $key: $value </div>";
+    //     }
+    //     echo '<br>';
+    // }
+    echo '</div>';
 ?>
     
 </body>
